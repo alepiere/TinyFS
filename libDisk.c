4bd14dbbaed1bcd2e35862ca4a04ddf74e753b6d
annielepiere <annalepiere@gmail.com>
2024-03-11 22:18:09 -0700

    openDisk

--- libDisk.c
+++ libDisk.c
@@ -11,14 +11,19 @@ int openDisk(char *filename, int nBytes){
         fprintf(stderr, "Error: Disk size must be at least BLOCKSIZE bytes.\n");
         return -1;
     }
+
+    off_t diskSize = nBytes - (nBytes % BLOCKSIZE);
+    if (diskSize == 0) {
+        diskSize = BLOCKSIZE; // Ensure disk size is at least BLOCKSIZE bytes
+    }
+
+    // Open file
     if (nBytes == 0) {
         // Open existing file without truncating
         fd = open(filename, O_RDWR);
     } else {
         // Open file with truncation to specified size
         fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
-        lseek(fd, nBytes, SEEK_SET);
-        write(fd, "\n", 1);
     }
 
     // Check if file open was successful
@@ -26,11 +31,23 @@ int openDisk(char *filename, int nBytes){
         perror("Error opening file");
         return -1;
     }
-    
+
+    // Truncate file size to calculated disk size
+    if (ftruncate(fd, diskSize) == -1) {
+        perror("Error truncating file");
+        close(fd);
+        return -1;
+    }
+
     return fd;
 }
 
-int closeDisk(int disk); /* self explanatory */
+int closeDisk(int disk){
+   if (fcntl(disk, F_GETFD) != -1) {
+      close(disk);
+   }
+   return 0;
+}
 
 int readBlock(int disk, int bNum, void *block);
 
